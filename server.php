ee35f57a36d1581afb3824ece3d55cff14571004
alwin <limwenzheng0508@gmail.com>
2025-10-27 10:13:16 +0800

    Creating the websocket

---
+++ server.php
@@ -0,0 +1,52 @@
+<?php
+  
+require_once __DIR__. '/vendor/autoload.php';
+
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
+class Chat implements MessageComponentInterface{
+    protected $clients;
+
+    public function __construct(){
+        echo "Chart server started...\n";
+    }
+
+
+    public function onOpen(ConnectionInterface $conn){
+        $this->clients->attach($conn);
+        echo "New connection! ({$conn->resourceId})\n";
+    }
+
+    public function onMessage(ConnectionInterface $from,$msg){
+        foreach ($this->clients as $client){
+            if($from !== $client){
+                $client->send($msg);
+            }
+        }
+        echo "Message received: $msg\n";
+    }
+
+    public function onClose(ConnectionInterface $conn){
+        $this->clients->detach($conn);
+        echo "Connection {$conn->resourceId} has disconnected\n";
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e){
+        echo "An error has occurred: {$e->getMessage()}\n";
+        $conn->close();
+    }
+
+}
+
+$server = \Ratchet\Server\IoServer::factory(
+    new \Ratchet\Http\HttpServer(
+        new \Ratchet\WebSocket\WsServer(
+            new Chat()
+        )
+    ),
+    8080
+);
+
+$server->run();
+?>
